3b828986058518bcb0ed8825151086f3b30f2480
Vigneshwarrs <vigneshwarrs@outlook.com>
2024-08-31 10:45:05 +0530

    Added backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://fittrack-backend-mu3q.onrender.com/',
+  baseURL: 'https://fittrack-backend-mu3q.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
