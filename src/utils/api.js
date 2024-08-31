df26c2dae8cb73d6068f375e1ad979eeadf35a26
Vigneshwarrs <vigneshwarrs@outlook.com>
2024-08-31 21:48:07 +0530

    3rd Stage

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://fittrack-backend-mu3q.onrender.com/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fittrack-backend-mu3q.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
